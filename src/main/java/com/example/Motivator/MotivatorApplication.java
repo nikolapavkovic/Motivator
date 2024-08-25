0ffe0832cc5783ae4a79855b963c86a54a83f941
nikolapavkovic <nikanberko286@gmail.com>
2024-08-25 20:04:27 +0200

    Initialize Spring Boot Project

---
+++ src/main/java/com/example/Motivator/MotivatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Motivator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotivatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Motivator/MotivatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Motivator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotivatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
