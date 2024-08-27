fbcba14a234240dab7bbc2b8844fd9bfac2afe99
nikolapavkovic <nikanberko286@gmail.com>
2024-08-27 20:42:11 +0200

    Add new endpoints for quote management

--- src/main/java/com/example/Motivator/model/dto/mapper/QuoteMapper.java
+++ src/main/java/com/example/Motivator/model/dto/mapper/QuoteMapper.java
@@ -7,15 +7,24 @@ import com.example.Motivator.model.dto.QuoteDto;
 public class QuoteMapper {
 
     public static QuoteDto fromEntity(final QuoteEntity quoteEntity) {
-        QuoteDto addQuoteDto = new QuoteDto();
-        addQuoteDto.setContent(quoteEntity.getContent());
-        addQuoteDto.setAuthor(quoteEntity.getAuthor());
-        addQuoteDto.setType(quoteEntity.getType().name());
-        return addQuoteDto;
+        QuoteDto quoteDto = new QuoteDto();
+        quoteDto.setContent(quoteEntity.getContent());
+        quoteDto.setAuthor(quoteEntity.getAuthor());
+        quoteDto.setType(quoteEntity.getType().name());
+        return quoteDto;
     }
 
     public static QuoteEntity fromDto(final QuoteDto addQuoteDto) {
         QuoteEntity quoteEntity = new QuoteEntity();
+        quoteEntity.setContent(addQuoteDto.getContent());
+        quoteEntity.setAuthor(addQuoteDto.getAuthor());
+        quoteEntity.setType(QuoteType.valueOf(addQuoteDto.getType()));
+        return quoteEntity;
+    }
+
+    public static QuoteEntity fromDto(final QuoteDto addQuoteDto, final Long id) {
+        QuoteEntity quoteEntity = new QuoteEntity();
+        quoteEntity.setId(id);
         quoteEntity.setContent(addQuoteDto.getContent());
         quoteEntity.setAuthor(addQuoteDto.getAuthor());
         quoteEntity.setType(QuoteType.valueOf(addQuoteDto.getType()));
--- src/main/java/com/example/Motivator/service/QuoteService.java
+++ src/main/java/com/example/Motivator/service/QuoteService.java
@@ -1,11 +1,16 @@
 package com.example.Motivator.service;
 
 import com.example.Motivator.entity.QuoteEntity;
+import com.example.Motivator.model.dto.QuoteDto;
+import com.example.Motivator.model.dto.mapper.QuoteMapper;
 import com.example.Motivator.repository.QuoteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,13 +18,54 @@ public class QuoteService {
 
     private final QuoteRepository quoteRepository;
 
-    public QuoteEntity getQuote(Integer id){
+    public QuoteEntity getQuote(Integer id) {
         Optional<QuoteEntity> quoteEntity = quoteRepository.findById(id);
         return quoteEntity.orElse(null);
     }
 
-    public void addQuote(QuoteEntity quoteEntity){
+    public void addQuote(QuoteEntity quoteEntity) {
         quoteRepository.save(quoteEntity);
+    }
+
+    public void deleteQuote(Integer id) {
+        quoteRepository.deleteById(id);
+    }
+
+    public QuoteDto updateQuote(Long id, QuoteDto quoteDto) {
+        return QuoteMapper.fromEntity(quoteRepository.save(QuoteMapper.fromDto(quoteDto, id)));
+    }
+
+    public List<QuoteDto> getAllQuotesByAuthor(String author) {
+        List<QuoteEntity> quoteEntities = quoteRepository.findAllByAuthor(author);
+        List<QuoteDto> quoteDtos = new ArrayList<>();
+
+        for (QuoteEntity quoteEntity : quoteEntities) {
+            quoteDtos.add(QuoteMapper.fromEntity(quoteEntity));
+        }
+        return quoteDtos;
+    }
+
+    public List<QuoteDto> getAllContaining(String text) {
+        List<QuoteEntity> quoteEntities = quoteRepository.findAllByContentContaining(text);
+
+        List<QuoteDto> quoteDtos = new ArrayList<>();
+
+        for (QuoteEntity quoteEntity : quoteEntities) {
+            quoteDtos.add(QuoteMapper.fromEntity(quoteEntity));
+        }
+
+        return quoteDtos;
+    }
+
+    public List<String> getAllAuthors() {
+        List<QuoteEntity> quoteEntities = quoteRepository.findAll();
+        List<String> authors = new ArrayList<>();
+
+        for (QuoteEntity quoteEntity : quoteEntities) {
+            authors.add(quoteEntity.getAuthor());
+        }
+
+        return authors.stream().distinct().collect(Collectors.toList());
     }
 
 }
